3f0b3cd1972301e8752bf53aed0ab8354d3846c4
mjkfriends <jayakumar.manian@gmail.com>
2016-06-27 12:45:13 +0530

    Find all the Links on a Webpage

---
+++ SeleniumExamples/src/com/smartplough/selenium/Linkcount.java
@@ -0,0 +1,35 @@
+package com.smartplough.selenium;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.MarionetteDriver;
+
+public class Linkcount {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\Mjk_Lab\\Automation\\geckodriver.exe");
+		WebDriver driver = new MarionetteDriver();
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.get("https://www.google.com/");
+	    driver.findElement(By.id("lst-ib")).clear();
+	    driver.findElement(By.id("lst-ib")).sendKeys("jayakumar manian");
+	    driver.findElement(By.name("btnG")).click();
+	    Thread.sleep(10000);
+	    
+	    List<WebElement> Links = driver.findElements(By.tagName("a"));
+	    Links.size();
+	    System.out.println("'Total no of links'"+Links.size());
+	    for(int i=0;i<Links.size();i++)
+	    {
+	    	System.out.println("Link Name = "+Links.get(i).getText());
+	    	System.out.println("URL ="+Links.get(i).getAttribute("href"));
+	   	}
+   }
+}
+
